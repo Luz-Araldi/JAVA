1f3862b3a4f5daa68aeab6b2f60675a109f3d7f2
Luz-Araldi <devluzaraldi@gmail.com>
2024-08-09 00:06:06 -0300

    JAVA-TRILHA-CONTROLE-FLUXO-DIO

---
+++ AboutMe.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+import.java.util.Locate;
+import.java.util.Scanner;
+
+public class AboutMe {
+    
+    public static void main(String[] args) {
+        //criando o objeto scanner 
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        string sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+        
+    }
+
+
+
+}
